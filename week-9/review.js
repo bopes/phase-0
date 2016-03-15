f04b85b6182979b3d6497ccc63c71e5ab50211bc
bopes <clcblee@gmail.com>
2016-03-15 12:25:15 -0400

    Clean up phase-0 directory

--- week-9/review.js
+++ week-9/review.js
@@ -2,7 +2,7 @@
 
 // Pseudocode
 
-// 2. Create a method that accepts an integer as an argument
+// 1. Create a method that accepts an integer as an argument
 //   a. Createa a starter array with 0 and 1
 //   b. Create a sum variable
 //   c. Create a while loop that runs while sum is less than the argument
@@ -22,9 +22,20 @@ function fibonacci(num) {
   return sum == num
 };
 
-Reflection
+// Reflection
 
-What concepts did you solidify in working on this challenge?
-What was the most difficult part of this challenge?
-Did you solve the problem in a new way this time?
-Was your pseudocode different from the Ruby version? What was the same and what was different?
+// What concepts did you solidify in working on this challenge?
+
+// I solidified bset practices writing pseudocode and accessing array element in JS
+
+// What was the most difficult part of this challenge?
+
+// The most difficult part was switching to JavaScript syntax and methods after using Ruby.
+
+// Did you solve the problem in a new way this time?
+
+// No, I kept the same approach. I do like how it turned out.
+
+// Was your pseudocode different from the Ruby version? What was the same and what was different?
+
+// Psuedocode was a little more structured and is more closely aligned to the final code.
